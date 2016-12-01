a08417096f43eaead0747c2768e8a53a47bb5200
Erin Mahon <erin.mahon14@gmail.com>
2016-12-01 02:11:39 -0500

    Fixed layout issue for task list section

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,8 @@
 var app = angular.module('toDoApp', []);
-app
+app // Handler function in scope to add ToDo object to the array of ToDo tasks of a tasklist
+
   .controller('ToDoController', ['$scope', function ($scope) {
-
-// Handler function in scope to add ToDo object to the array of ToDo tasks of a tasklist
-
-  $scope.tasks = [];
+    $scope.tasks = [];
     $scope.editIndex = false;
     $scope.addTask = function () {
         if( $scope.editIndex === false){
@@ -15,9 +13,7 @@ app
         $scope.editIndex = false;
         $scope.task = '';
     }
-
-    // Handler function in scope to edit, delete, and mark ToDo tasks as done
-        
+  	// Handler function in scope to edit, delete, and mark ToDo tasks as done      
     $scope.editTask = function (index) {
       $scope.task = $scope.tasks[index].task;
       $scope.editIndex = index;
@@ -31,5 +27,6 @@ app
     $scope.deleteTask = function (index) {
       $scope.tasks.splice(index, 1);
     }
-  }])
 
+
+  }])
