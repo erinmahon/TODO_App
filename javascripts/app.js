c6c0b19eba4d496b04c791d2d9459c3cc87e9ef5
Erin Mahon <erin.mahon14@gmail.com>
2016-12-01 14:47:28 -0500

    Styling of todo list, added number counter to task items

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,11 @@
 var app = angular.module('toDoApp', []);
-app // Handler function in scope to add ToDo object to the array of ToDo tasks of a tasklist
-
-  .controller('ToDoController', ['$scope', function ($scope) {
+app.controller('ToDoController', ['$scope', function ($scope)  {
+   
     $scope.tasks = [];
+    $scope.lists = [];
     $scope.editIndex = false;
+    // Handler functions in scope
+    // add task
     $scope.addTask = function () {
         if( $scope.editIndex === false){
             $scope.tasks.push({task: $scope.task, done: false})
@@ -13,20 +15,57 @@ app // Handler function in scope to add ToDo object to the array of ToDo tasks o
         $scope.editIndex = false;
         $scope.task = '';
     }
-  	// Handler function in scope to edit, delete, and mark ToDo tasks as done      
+    //add list
+    $scope.addList = function () {
+        if( $scope.editIndex === false){
+            $scope.lists.push({list: $scope.list, done: false})
+        } else {
+            $scope.lists[$scope.editIndex].list = $scope.list;
+        }
+        $scope.editIndex = false;
+        $scope.list = '';
+    }
+    // edit task     
     $scope.editTask = function (index) {
       $scope.task = $scope.tasks[index].task;
       $scope.editIndex = index;
     }
+    // mark task as completed
     $scope.doneTask = function (index) {
       $scope.tasks[index].done = true;
     }
-    $scope.unDoneTask = function (index) {
-      $scope.tasks[index].done = false;
-    }
+
+    // delete task
     $scope.deleteTask = function (index) {
       $scope.tasks.splice(index, 1);
     }
 
+    //delete list
+    $scope.deleteList = function (index) {
+      $scope.lists.splice(index, 1);
+    }
 
-  }])
+    }])
+
+$(document).ready(function(){
+    
+    $("#new-list-btn").click(function() {
+        $("#new-list-form").removeClass('hidden');
+        $("#add-new-btn").hide();
+    })
+
+    $("#list-name").click(function() {
+        $("#add-new-btn").show();
+    })
+
+    $("#add-new-btn").click(function() {
+        $("#new-list-form").hide();
+    })
+
+
+});
+
+  
+
+
+
